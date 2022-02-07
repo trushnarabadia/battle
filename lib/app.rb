e4ef924407511ac675b97d0df6fbcd99f4bb4de5
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-02-07 17:53:44 +0000

    sinatra: view

--- lib/app.rb
+++ lib/app.rb
@@ -1,17 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-
-# configure :production do
-#   enable :reloader
-# end
-
-get '/' do
-  "Hello!"
-end
-
 get '/cat' do
-  "<div>
-    <img src=”https://i.imgur.com/jFaSxym”>
-   </div>"   
+  erb(:index)
 end
