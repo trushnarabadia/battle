867f5aca846b9b275b7fba99f9e0fc6d4de77837
Ben Partington <bdpartington@gmail.com>
2022-02-08 17:10:15 +0000

    Sinatra: POSTed params

--- lib/app.rb
+++ lib/app.rb
@@ -3,11 +3,15 @@ require 'sinatra/reloader' if development?
 
 get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
