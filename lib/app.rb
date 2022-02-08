0e5511b1e74477322fc6862cc10138e14187b271
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-02-08 15:20:18 +0000

    introducing params

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
